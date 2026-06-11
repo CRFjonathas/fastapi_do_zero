4b1e37e0a200ecc5702739bfe4a433425b0bda5c
CRFjonathas <jonaths.github@gmail.com>
2026-06-11 13:33:13 -0300

    feat: adiciona modelo Message e ajusta resposta da rota raiz

--- fastapi_do_zero/app.py
+++ fastapi_do_zero/app.py
@@ -1,9 +1,12 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fastapi_do_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/', status_code = HTTPStatus.OK)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'Mensagem': 'Olá, Mundo!'}
+    return {'message': 'Olá, Mundo!'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_do_zero.app import app
@@ -9,5 +10,5 @@ def test_root_deve_retornar_ola_mundo():
 
     response = client.get('/')
 
-    assert response.json() == {'Mensagem': 'Olá, Mundo!'}
+    assert response.json() == {'message': 'Olá, Mundo!'}
     assert response.status_code == HTTPStatus.OK
